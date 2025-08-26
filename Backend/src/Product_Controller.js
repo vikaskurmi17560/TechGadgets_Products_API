bf9212e7914ba8695f6c6240b715f4ccf5df9072
vikaskurmi17560 <vkurmi307@gmail.com>
2025-08-26 21:58:07 +0530

    create new controller for get single product and Router

--- Backend/src/Product_Controller.js
+++ Backend/src/Product_Controller.js
@@ -30,3 +30,25 @@ exports.getAllProducts = async (req, res) => {
         });
     }
 }
+
+exports.getSignleProduct= async (req,res)=>{
+    try{
+        const product = await Products.findById(req.query._id);
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: product,
+            message: "Product retrieved successfully"
+        });
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- Backend/src/Product_Routes.js
+++ Backend/src/Product_Routes.js
@@ -1,7 +1,8 @@
 require("dotenv").config(); 
 const express = require("express");
 const Router = express.Router();
-const { createProduct, getAllProducts } = require("./Product_Controller");
+const { createProduct, getAllProducts,getSignleProduct } = require("./Product_Controller");
 Router.post(process.env.ROUTE_PREFIX + "/create", createProduct);
 Router.get("/", getAllProducts);
+Router.get("/singleProduct",getSignleProduct);
 module.exports = Router;
