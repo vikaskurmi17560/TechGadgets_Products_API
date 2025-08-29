b060f6a7853c06693e10a96ceffc10867df2ecbd
vikaskurmi17560 <vkurmi307@gmail.com>
2025-08-29 19:49:19 +0530

    corrcet code

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors"); // <-- Add this line
 const app = express();
+
+app.use(cors()); // <-- Enable CORS for all routes
 app.use(express.json());
+
 const Product_Routes = require("./src/Product_Routes");
 
+// Use /products as the endpoint for product routes
 app.use("/", Product_Routes);
 
 module.exports = app;
